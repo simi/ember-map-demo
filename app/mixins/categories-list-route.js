16a5e7e1f0145aa9643ad37b33272046a67ad3d3
Josef Šimánek <josef.simanek@gmail.com>
2014-11-27 23:35:32 +0100

    JSLint for live.

--- app/mixins/categories-list-route.js
+++ app/mixins/categories-list-route.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Mixin.create({
-  renderTemplate: function(controller, model) {
+  renderTemplate: function(controller) {
     this.render('categories.list', {
       controller: controller
     });
